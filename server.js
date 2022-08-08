22ee52ecad8b8fcc2d8b0afd140331a5cfb6d7cb
valcomp <valerie.aleida.lopez@gmail.com>
2022-08-07 19:23:47 -0500

    created server.js file

---
+++ server.js
@@ -0,0 +1,22 @@
+//initialize express
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//add middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+//to use our css files
+app.use(express.static('public'));
+
+//use api routes
+app.use('/api',apiRoutes);
+app.use('/',htmlRoutes);
+
+//to make server listen
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
