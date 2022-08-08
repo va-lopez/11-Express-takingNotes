9cf5a7df33762c592cd578a7d83f18f7771c8988
valcomp <valerie.aleida.lopez@gmail.com>
2022-08-08 09:01:32 -0500

    added delete route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,16 +1,30 @@
 const router = require('express').Router();
-const {createNewNote, validateNote} = require('../../lib/notes')
+const {findByIdAndDelete, createNewNote, validateNote} = require('../../lib/notes');
+var uniqid = require('uniquid');
 
 //db where notes are stored
 const { notes } = require('../../db/db.json');
 
+//reads the db.json file
 router.get('/notes', (req,res) => {
     let results = notes;
     res.json(results);
 });
 
+//get by id
+router.delete('/notes/:id', (req,res) => {
+    results = findByIdAndDelete(req.params.id,notes);
+    if(results){
+        res.json(results);
+    }else{
+        res.send(404);
+    }
+});
+
+//post a new note into db
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+    //generates a unique id
+    req.body.id = uniqid();
 
     if(!validateNote(req.body)) {
         res.status(400).send('The note is not properly filled out.');
